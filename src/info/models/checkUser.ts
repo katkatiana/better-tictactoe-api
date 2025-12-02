86a4776ff1b090e6f24f09600c50633b2f12c6a7
Mariakatia Santangelo <mariakatia.santangelo@generali.com>
2025-12-02 23:43:46 +0100

    refactored error messages

--- src/info/models/checkUser.ts
+++ src/info/models/checkUser.ts
@@ -1,24 +1,44 @@
-import { IsBoolean, IsDateString, IsNotEmpty, IsNumber, IsString, Max, MaxLength, Min, MinLength, ValidateIf } from "class-validator";
+import { IsNotEmpty, Max, MaxLength, Min, MinLength, ValidateIf } from "class-validator";
 import { UserInfoRequest } from "../interfaces";
 import { AgeMatchesDate } from "../decorators/age-matches-date.decorator";
 
+enum ErrorMsg {
+    EMPTY_NAME = "Il nome non puo' essere vuoto.",
+    MIN_LENGTH_NAME = "Il nome deve essere lungo almeno XXX caratteri.",
+    MAX_LENGTH_NAME = "Il nome non puo' superare i XXX caratteri.",
+    MIN_AGE = "L'eta' non puo' essere inferiore a XXX.",
+    MAX_AGE = "L'eta' non puo' superare XXX.",
+    MARRIED_MANDATORY = "La selezione e' obbligatoria per maggiorenni.",
+    EMPTY_DATE = "La data di nascita non puo' essere vuota.",
+    AGE_MATCHES_DATE = "L'eta' non corrisponde alla data di nascita inserita."
+}
+
 export class UpdateUserInfoRequest implements UserInfoRequest {
 
-    //TO DO: aggiungere messaggi di errore non statici
-    @IsNotEmpty({ message: "Il nome non può essere vuoto" })
-    @MinLength(5, { message: "Il nome deve essere lungo almeno 5 caratteri" })
-    @MaxLength(50, { message: "Il nome non può superare i 50 caratteri" })
+    static readonly USERNAME_MIN_LENGTH = 5;
+    static readonly USERNAME_MAX_LENGTH = 50;
+    static readonly AGE_MIN_VALUE = 1;
+    static readonly AGE_MAX_VALUE = 150;
+    static readonly AGE_THRESHOLD = 18;
+
+    @IsNotEmpty({ message: UpdateUserInfoRequest.buildErrorMessage(ErrorMsg.EMPTY_NAME) })
+    @MinLength(UpdateUserInfoRequest.USERNAME_MIN_LENGTH, { message: UpdateUserInfoRequest.buildErrorMessage(ErrorMsg.MIN_LENGTH_NAME, UpdateUserInfoRequest.USERNAME_MIN_LENGTH) })
+    @MaxLength(UpdateUserInfoRequest.USERNAME_MAX_LENGTH, { message: UpdateUserInfoRequest.buildErrorMessage(ErrorMsg.MAX_LENGTH_NAME, UpdateUserInfoRequest.USERNAME_MAX_LENGTH) })
     username: string;
 
-    @Min(1, { message: "L'età minima è 1 anno" })
-    @Max(150, { message: "L'età massima è 150 anni" })
+    @Min(UpdateUserInfoRequest.AGE_MIN_VALUE, { message: UpdateUserInfoRequest.buildErrorMessage(ErrorMsg.MIN_AGE, UpdateUserInfoRequest.AGE_MIN_VALUE) })
+    @Max(UpdateUserInfoRequest.AGE_MAX_VALUE, { message: UpdateUserInfoRequest.buildErrorMessage(ErrorMsg.MAX_AGE, UpdateUserInfoRequest.AGE_MAX_VALUE) })
     age: number
 
-    @ValidateIf((o) => o.age >= 18)
-    @IsNotEmpty({ message: "La selezione è obbligatoria per maggiorenni" })
+    @ValidateIf((o) => o.age >= UpdateUserInfoRequest.AGE_THRESHOLD)
+    @IsNotEmpty({ message: UpdateUserInfoRequest.buildErrorMessage(ErrorMsg.MARRIED_MANDATORY) })
     married: boolean;
 
-    @IsNotEmpty({ message: "La data di nascita non può essere vuota" })
-    @AgeMatchesDate({ message: "L'età non corrisponde alla data di nascita inserita" })
+    @IsNotEmpty({ message: UpdateUserInfoRequest.buildErrorMessage(ErrorMsg.EMPTY_DATE) })
+    @AgeMatchesDate({ message: UpdateUserInfoRequest.buildErrorMessage(ErrorMsg.AGE_MATCHES_DATE) })
     dateOfBirth: string;
+
+    static buildErrorMessage(msg: string, replacementValue?: number): string {
+        return replacementValue ? msg.replace("XXX", replacementValue.toString()) : msg;
+    }
 }
